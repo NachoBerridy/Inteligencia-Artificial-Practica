dcb0431b13a640f49c77dca75412c87e791c3109
NachoBerridy <nachoberridy1997@gmail.com>
2022-04-10 17:20:18 -0300

    cambio nombre de variables

--- TP1/Ejercicio_1/path.py
+++ TP1/Ejercicio_1/path.py
@@ -1,4 +1,5 @@
 from cmath import sqrt
+from msilib import sequence
 from layout import Layout
 class Path:
 
@@ -7,68 +8,68 @@ class Path:
         self.total_cost:int
         self.storage:Layout
 
-    def a_star(self,p_inicial,c_fila,c_col,target):
-        nodos_abiertos =[]
-        nodos_cerrados=[]
-        puntos=[]
-        camino=[]
-        nodos_cerrados.append(p_inicial)
-        (x,y)=p_inicial
+    def a_star(self,starting_point,c_row,c_col,target):
+        open_nodes =[]
+        closed_nodes=[]
+        neighbours=[]
+        sequence=[]
+        closed_nodes.append(starting_point)
+        (x,y)=starting_point
         while (x,y)!=target: ##nodo objetivo
 
             root = (x,y) #nodo raiz
-            puntos.clear()
+            neighbours.clear()
 
             if   y==0: 
                 if 0<x<c_col: #c_col: cantidad de columnas
-                    puntos = [(x,y+1),(x+1,y),(x-1,y)]
+                    neighbours = [(x,y + 1),(x+1,y),(x-1,y)]
                 elif x==0:
-                    puntos = [(x+1,y),(x,y+1)]
+                    neighbours = [(x+1,y),(x,y+1)]
                 elif x==c_col:
-                    puntos = [(x,y+1),(x-1,y)]            
+                    neighbours = [(x,y+1),(x-1,y)]            
 
             elif x==0:  
-                if  0<y<c_fila:
-                    puntos = [(x+1,y),(x,y-1),(x,y+1)]
-                elif y==c_fila:
-                    puntos = [(x,y-1),(x+1,y)]
+                if  0<y<c_row:
+                    neighbours = [(x+1,y),(x,y-1),(x,y+1)]
+                elif y==c_row:
+                    neighbours = [(x,y-1),(x+1,y)]
 
             elif x ==c_col:
-                if 0<y<c_fila:
-                    puntos = [(x-1,y),(x,y+1),(x,y-1)]
-                elif y==c_fila:
-                    puntos = [(x-1,y),(x,y-1)]
+                if 0<y<c_row:
+                    neighbours = [(x-1,y),(x,y+1),(x,y-1)]
+                elif y==c_row:
+                    neighbours = [(x-1,y),(x,y-1)]
 
-            elif y==c_fila:  
+            elif y==c_row:  
                 if 0<x<c_col:
-                    puntos = [(x+1,y),(x-1,y),(x,y-1)]
+                    neighbours = [(x+1,y),(x-1,y),(x,y-1)]
             else: 
-                puntos = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+                neighbours = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
 
-            for i in puntos: 
-                if self.storage.mat[i]==False or i in nodos_cerrados:
-                    puntos.remove(i)
-            for i in puntos:
-                if i in nodos_abiertos:
+            for i in neighbours: 
+                if self.storage.mat[i]==False or i in closed_nodes:
+                    neighbours.remove(i)
+            for i in neighbours:
+                if i in open_nodes:
                     if self.storage.mat[i].g>self.storage.mat[x,y].g+1: 
                         self.storage.mat[i].root=root
                         self.storage.mat[i].g=self.storage.mat[x,y].g+1
                         self.storage.mat[i].f=self.storage.mat[i].h+self.storage.mat[i].g
                 else:
-                    nodos_abiertos.append(i)
+                    open_nodes.append(i)
                     self.storage.mat[i].root=root
                     self.storage.mat[i].h=round(sqrt(((target[0]-i[0])**2)+((target[1]-i[1])**2)))
                     self.storage.mat[i].g=self.storage.mat[x,y].g+1
                     self.storage.mat[i].f=self.storage.mat[i].h+self.storage.mat[i].g
-            (x,y)=nodos_abiertos[0]
-            for i in nodos_abiertos:
+            (x,y)=open_nodes[0]
+            for i in open_nodes:
                 if (self.storage.mat[x,y].f>self.storage.mat[i].f):
                     (x,y) = i
-            nodos_cerrados.append((x,y))
-            nodos_abiertos.remove((x,y))
-        while target != p_inicial:
-            camino.append(target)
+            closed_nodes.append((x,y))
+            open_nodes.remove((x,y))
+        while target != starting_point:
+            sequence.append(target)
             target=self.storage.mat[target].root
-        camino_ordenado=reversed(camino)
+            ordered_sequence=reversed(sequence)
 
-        return camino_ordenado
+        return ordered_sequence

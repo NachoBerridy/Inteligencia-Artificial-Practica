72e7d2c3f89ba68ab926cbc5e3519f188ae0d3da
NachoBerridy <nachoberridy1997@gmail.com>
2022-04-14 18:49:48 -0300

    arreglo grid

--- TP1/Ejercicio_1/interfaz_2.py
+++ TP1/Ejercicio_1/interfaz_2.py
@@ -52,8 +52,8 @@ def main_loop(path1):
         screen.fill((148, 148, 148))
         (x,y) = (0,1)
 
-        for i in range(path1.storage.rows-1):
-            for j in range(path1.storage.columns-1):
+        for i in range(path1.storage.rows):
+            for j in range(path1.storage.columns):
                 if path1.storage.mat[i,j].is_rack == True:
                     rect(screen, path1.storage.mat[i,j].color, [x, y,tamaño_cuadro, tamaño_cuadro])
                 elif path1.storage.mat[i,j].is_rack == False:

64226f98b7a3c36b1116bef4e7bad98032858222
NachoBerridy <nachoberridy1997@gmail.com>
2022-04-20 22:01:58 -0300

    fix conflict

--- TP1/Ejercicio_1/main.py
+++ TP1/Ejercicio_1/main.py
@@ -6,13 +6,6 @@ from interfaz_1 import*
 
 if __name__ == "__main__":
 
-<<<<<<< HEAD
-    layout_1 = Layout(8,7,0,4,4)
-    layout_1.fill_mat()
-    path_1=Path(layout_1)
-    main_loop(path_1)
-=======
     gui1=Gui()
     gui1.root.mainloop()
     main_loop(gui1.path1)
->>>>>>> 13e6911dcdad92166f759b70267ad1b57fdb2378

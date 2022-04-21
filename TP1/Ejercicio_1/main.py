0328725bc8fddf7c1fcba8be9c25cd0a771f6ea2
NachoBerridy <nachoberridy1997@gmail.com>
2022-04-20 22:00:04 -0300

    0

--- TP1/Ejercicio_1/main.py
+++ TP1/Ejercicio_1/main.py
@@ -5,7 +5,7 @@ from interfaz_2 import*
 
 if __name__ == "__main__":
 
-    cuadro = Layout(8,7,0,4,4)
-    cuadro.fill_mat()
-    path1=Path(cuadro)
-    main_loop(path1)
+    layout_1 = Layout(8,7,0,4,4)
+    layout_1.fill_mat()
+    path_1=Path(layout_1)
+    main_loop(path_1)

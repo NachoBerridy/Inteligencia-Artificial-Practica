83f2806898390757b0ee8a8e855dd5c1732a27c9
AntoAldao <antoaldao@gmail.com>
2022-04-27 13:30:21 -0300

    funcion fitness

--- TP1/Ejercicio_3/genetic_algorithm.py
+++ TP1/Ejercicio_3/genetic_algorithm.py
@@ -9,28 +9,29 @@ class Genetic_Algorithm:
         self.fitness_list = []
         self.probability = []
         self.total_fitness = 0
+        self.orders = []
 
     def read_txt(self):
 
         orders=open("orders.txt","r")
-        a=1
-        list_1=[]
-        list_2=[]
-        list_3=[]
-        counter=0
+        a = 1
+        list_1 = []
+        list_2 = []
+        list_3 = []
+        counter = 0
         for i in orders:
             list_1.append(i) #txt en una lista
             if i==("Order %s\n"%(a)):
-                a=a+1
+                a = a + 1
                 list_2.append(counter) #puntos donde cortar las lista del txt
-            counter=counter+1
+            counter = counter + 1
 
         for i in range(len(list_2)-1):
             list_3.append(list_1[list_2[i]+1:list_2[i+1]-1])
         list_3.append(list_1[list_2[len(list_2)-1]+1:-1])
         
-        list_1=[]
-        list_2=[]
+        list_1 = []
+        list_2 = []
 
         for i in range(len(list_3)):
             for j in range(len(list_3[i])):
@@ -39,7 +40,7 @@ class Genetic_Algorithm:
             list_1.append(list_2)
             list_2=[]
         
-        return list_1
+        self.orders = list_1[:]
 
     def child_complete(self,father,child,pos): 
         aux=[]
@@ -102,22 +103,24 @@ class Genetic_Algorithm:
             child[pos1],child[pos2]=child[pos2],child[pos1]
         return child
 
-    def fitness(self,lista_layout): #lista layout es uno de los individuos de la poblacion, osea una configuracion del layout
+    def fitness(self,list_layout): #lista layout es uno de los individuos de la poblacion, osea una configuracion del layout
         fitness = 0
-        for i in lista_layout:
-            s = Simulated_Annealing(i)
-            s.sequence(i)
-
-
+        for i in self.orders:
+            s = Simulated_Annealing(i,list_layout)
+            s.fill_dicts()
+            fitness = fitness + s.sequence()[0]
+            del s 
+        
         return fitness
-    
+
+
     def selec_parents(self):
         pass
 
     def optimal_layout(self):
         while p<50000:
-            index_list=[]
-            children_list=[]
+            index_list = []
+            children_list = []
             
             #lista del fitness
             for i in range(len(self.population)):
@@ -125,7 +128,7 @@ class Genetic_Algorithm:
 
             #fitness total
             for i in range(len(self.fitness_list)):
-                self.total_fitness=self.total_fitness+self.fitness_list[i]
+                self.total_fitness = self.total_fitness + self.fitness_list[i]
 
             #lista de probabilidades de cada individuo
             for i in range(len(self.fitness_list)):
@@ -133,11 +136,11 @@ class Genetic_Algorithm:
 
             #seleccion 
             for i in range(int(len(self.population/2))):
-                father1,index_1=self.selec_parents()
-                father2=father1[:]
-                index_2=index_1
+                father1,index_1 = self.selec_parents()
+                father2 = father1[:]
+                index_2 = index_1
                 while father2==father1 and (index_1,index_2) in index_1:
-                    father2=self.selec_parents()
+                    father2 = self.selec_parents()
 
                 index_list.append(index_1,index_2)
                 children_list.extend(self.crossover(father1,father2))
@@ -146,4 +149,4 @@ class Genetic_Algorithm:
 
             #nueva poblacion
             self.population=children_list[:]
-            p=p+1
+            p = p + 1

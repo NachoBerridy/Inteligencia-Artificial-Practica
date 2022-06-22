d0952d05ee3ff1ece1a0040e64df2c6d4a1c8836
NachoBerridy <nachoberridy1997@gmail.com>
2022-06-22 12:43:03 -0300

    Ejercicio 2

---
+++ TP3/a.py
@@ -0,0 +1,29 @@
+from itertools import count
+import numpy as np
+
+"""a = np.linspace(1,3,3)
+b = np.linspace(4,6,3)
+d = np.linspace(0,15,16)
+c = range(0,5,1)
+e = range(5,10,1)
+#print(d[c])
+#print(d[e])
+randomgen = np.random.default_rng()
+
+w1 = randomgen.standard_normal((2, 5))
+r = np.size(0,w1)
+print(r)"""
+
+lista = [0,1,1,2,1,0,2]
+l = [0,2,1,2,1,0,2]
+a =lista-l
+print(a)
+print(a.count(0))
+
+print(lista.count(1))
+
+#print(w1)
+"""for i in c:
+    print(i)
+print(c)
+print(a*b)"""
